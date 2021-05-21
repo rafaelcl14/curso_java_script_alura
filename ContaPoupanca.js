2b8f52c6d451b3c6d913590103a25d3f3832eafd
Rafael Carvalho Rosina <39815593+rafaelcl14@users.noreply.github.com>
2021-05-21 09:45:49 -0400

    Primeiro Commit

---
+++ ContaPoupanca.js
@@ -0,0 +1,7 @@
+export class ContaPoupanca{
+    constructor(saldoInicial, cliente, agencia){
+        this._saldoInicial = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+}
